2eff7a2db9df7c5e1acd55f039b17d0a34b390db
Buu Gia Huynh <buub1309120@gmail.com>
2023-05-11 17:33:36 +0700

    next image tag, style up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,73 @@ import {
   UserButton,
   useUser,
 } from "@clerk/nextjs";
+import { log } from "console";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-import { api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex gap-2">
+      <Image
+        src={user.profileImageUrl}
+        alt="Photo"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        type="text"
+        placeholder="Type some emojis"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div className="flex w-full gap-4 border-b border-slate-400 p-4">
+      <Image
+        src={author.profileImageUrl}
+        alt="author image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-2">
+          <span>@{author.username}</span>
+          {" · "}
+          <span>{dayjs(post.createdAt).fromNow()}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
-  const { isLoaded, isSignedIn, user } = useUser();
+  const user = useUser();
 
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
+  const { data, isLoading } = api.posts.getAll.useQuery();
 
-  const { data } = api.posts.getAll.useQuery();
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!data) return <div>Something went wrong!</div>;
 
   return (
     <>
@@ -29,22 +82,33 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <header className="flex flex-col items-center justify-center gap-5 p-10 text-white">
-            <h1>My App</h1>
-            <SignedIn>
-              {/* Mount the UserButton component */}
-              <UserButton />
-              <div>Hello, {user.firstName} welcome to Clerk</div>
-            </SignedIn>
-            <SignedOut>
-              {/* Signed out users get sign in button */}
-              <SignInButton />
-            </SignedOut>
+        <div className="container flex flex-col items-center justify-center gap-12 border-x border-b border-slate-400  py-16">
+          <header className="flex flex-col items-center justify-center gap-5  p-10">
+            <div>
+              <h1>My App</h1>
+              <SignedIn>
+                {/* Mount the UserButton component */}
+                <UserButton />
+              </SignedIn>
+              <SignedOut>
+                {/* Signed out users get sign in button */}
+                <SignInButton />
+              </SignedOut>
+            </div>
+            {!user.isSignedIn && (
+              <div>
+                <SignInButton />
+              </div>
+            )}
+            {user.isSignedIn && (
+              <div>
+                <CreatePostWizard />
+              </div>
+            )}
           </header>
-          <div>
-            {data?.map((post) => (
-              <div key={post.id}>{post.content}</div>
+          <div className="flex w-full">
+            {data?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
