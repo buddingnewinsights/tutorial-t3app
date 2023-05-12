d914b6c79795fd5d09b7149281c3bd8e0aee9890
Buu Gia Huynh <buub1309120@gmail.com>
2023-05-12 10:35:39 +0700

    Profile page with SSG helpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,79 @@
-import { type NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
 
-const ProfilePage: NextPage = () => {
+import { api } from "~/utils/api";
+import { PageLayout } from "~/Components/layout";
+import Image from "next/image";
+
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
+
+  if (!data) return <div>404</div>;
+
+  console.log(username);
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col gap-5 border-x border-b border-slate-400 py-8">
-          <h1>Profile View</h1>
+      <Head>
+        <title>{data.username}</title>
+      </Head>
+
+      <PageLayout>
+        <div className="relative h-48 border-b border-black bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`@${data.username ?? ""}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black"
+          />
+          <div className="h-64"></div>
+          <div className="p-4 text-2xl font-bold">{`@${
+            data.username ?? ""
+          }`}</div>
+          <div className="border-b border-slate-400" />
         </div>
-      </main>
+      </PageLayout>
     </>
   );
 };
 
 export default ProfilePage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug?.replace("@", "");
+
+  // prefetch `post.byId`
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+    revalidate: 1,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
